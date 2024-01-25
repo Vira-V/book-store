b329545c83038b3e88c1b4de53e4efcc84c89e4f
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-01-25 17:51:16 +0100

    created data for blogs

---
+++ src/data.js
@@ -0,0 +1,34 @@
+import image1 from "../src/images/blog1.jpg";
+import image2 from "../src/images/blog2.jpg";
+import image3 from "../src/images/blog3.jpg";
+import image4 from "../src/images/blog4.jpg";
+
+export const data = {
+  title: "Read our many blogs",
+  blogs: [
+    {
+      title: "Learn about this week's top author",
+      text: "Jump start your book reading by quickly checking through the popular book categories...",
+      image: image1,
+      description: "The man with a cup",
+    },
+    {
+      title: "Why we celebrate readers",
+      text: "Jump start your book reading by quickly check through the popular book categories...",
+      image: image2,
+      description: "A girl and two men with drinks",
+    },
+    {
+      title: "Toddlers can also read",
+      text: "Jump start your book reading by quickly check through the popular book categories...",
+      image: image3,
+      description: "A toddler is laughing",
+    },
+    {
+      title: "Get started on your game",
+      text: "Jump start your book reading by quickly check through the popular book categories...",
+      image: image4,
+      description: "An open book",
+    },
+  ],
+};

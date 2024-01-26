4fd2d74e85eb66cf70b41ba59c80252b5a10aaec
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-01-26 17:33:34 +0100

    added button to blog component

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,15 +2,23 @@ import "./Blog.scss";
 
 export const Blog = ({ title, blogs }) => {
   return (
-    <div className="blog-container">
+    <>
       <h2 className="blog-title">{title}</h2>
-      {blogs.map((blog, id) => (
-        <div key={id} className="blog">
-          <img src={blog.image} alt={blog.description} className="blog__image" />
-          <h3 className="blog__title">{blog.title}</h3>
-          <p className="blog__text">{blog.text}</p>
-        </div>
-      ))}
-    </div>
+      <div className="blog-container">
+        {blogs.map((blog, id) => (
+          <div key={id} className="blog">
+            <img
+              src={blog.image}
+              alt={blog.description}
+              className="blog__image"
+            />
+            <div className="blog__overlay"></div>
+            <h3 className="blog__title">{blog.title}</h3>
+            <p className="blog__text">{blog.text}</p>
+            <button className="blog__read-more">Read more</button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };

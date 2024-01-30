6a005caf86c97b434b6e12d3be21dbe3f55df1b0
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-01-30 16:32:38 +0100

    added Header component and filled the first part with styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Subscription } from './components/Subscription/Subscription';
 import { data } from './data';
 import { Blog } from './components/Blog/Blog';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Subscription />
       <Blog title={data.title} blogs={data.blogs} />
     </>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+  faBloggerB,
+} from "@fortawesome/free-brands-svg-icons";
+
+export const Header = () => {
+  return (
+    <div className="top-header">
+      <div className="top-header__phone">
+        <FontAwesomeIcon icon={faPhone} className="top-header__phone-icon" />
+        <a href="tel: +91 8374902234" className="top-header__phone-link">
+          +91 8374902234
+        </a>
+      </div>
+      <div className="top-header__social">
+        <a href="https://www.facebook.com" className="top-header__social-icon">
+          <FontAwesomeIcon icon={faFacebookF} />
+        </a>
+        <a href="https://www.instagram.com" className="top-header__social-icon">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="https://www.linkedin.com" className="top-header__social-icon">
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+        <a href="https://twitter.com/" className="top-header__social-icon">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="https://www.blogger.com/" className="top-header__social-icon">
+          <FontAwesomeIcon icon={faBloggerB} />
+        </a>
+      </div>
+    </div>
+  );
+};

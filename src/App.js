b2b4ef50d63868baaf482363f3dd53a2bcf8eb6c
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-02-01 18:20:26 +0100

    created the next component Ebook

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Subscription } from './components/Subscription/Subscription';
 import { data } from './data';
 import { Blog } from './components/Blog/Blog';
 import { Header } from './components/Header/Top-header/Header';
+import { Ebook } from './components/EBook/Ebook';
 
 function App() {
   return (
     <>
       <Header />
+      <Ebook />
       <Subscription />
       <Blog title={data.title} blogs={data.blogs} />
     </>
---
+++ src/components/EBook/Ebook.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./Ebook.scss";
+
+export const Ebook = () => {
+  const [email, setEmail] = useState("");
+
+  const login = () => {
+    if (validateEmail(email)) {
+      alert("Login successful for " + email);
+      console.log("Email:", email);
+    } else {
+      alert("Please enter a valid email address.");
+    }
+  };
+
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  return (
+    <div className="ebook-container">
+      <div className="ebook">
+        <span className="ebook__category">ebook</span>
+        <h3 className="ebook__title">
+          Access, Read, Practice & Engage with Digital Content (eBook){" "}
+        </h3>
+        <p className="ebook__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu
+          feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut
+          magna velit eleifend. Amet, quis urna, a eu.Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit.
+        </p>
+        <form className="ebook__form">
+          <label htmlFor="ebook__email"></label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email address"
+            required
+          />
+          <a href="/" className="ebook__button" onClick={login}>
+            Login
+          </a>
+        </form>
+      </div>
+      <div className="image"></div>
+    </div>
+  );
+};

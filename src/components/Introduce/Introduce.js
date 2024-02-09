888f29cab2e060d7aeea89c63ff553a94b21b29e
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-02-09 17:31:24 +0100

    added styles to Introduce component

--- src/components/Introduce/Introduce.js
+++ src/components/Introduce/Introduce.js
@@ -4,17 +4,21 @@ import introduceImage from "../../images/introduce-image.jpg";
 export const Introduce = () => {
   return (
     <div className="introduce-container">
-        <div className="introduce">
+      <div className="introduce">
         <h3 className="introduce__title">Ipsum Dolor Si</h3>
-        <p className="introduce__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut magna velit eleifend. Amet, quis urna, a eu.</p>
-        <a href="/" className="view-button">
-        <span className="view-button__text">View more</span>
-      </a>
-        </div>
+        <p className="introduce__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu
+          feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut
+          magna velit eleifend. Amet, quis urna, a eu.
+        </p>
+        <a href="/" className="introduce__button view-button">
+          <span className="view-button__text">Read more</span>
+        </a>
+      </div>
 
-        <div className="category__text">
+      <div className="introduce-image">
         <img src={introduceImage} alt="Slider" />
-        </div>           
+      </div>
     </div>
   );
 };

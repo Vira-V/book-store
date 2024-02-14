b5f9498ab585576841e66f4e1c2c6d171bf358ac
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-02-14 18:35:43 +0100

    improved Release component and added responsive styles

--- src/components/ReleaseBooks/ReleaseBook.js
+++ src/components/ReleaseBooks/ReleaseBook.js
@@ -2,22 +2,26 @@ import "./ReleaseBook.scss";
 
 export const ReleaseBook = ({ books }) => {
   return (
-    <div className="release-container">
-      {books.map((book, id) => (
-        <div key={id} className="release">
-          <img
-            src={book.image}
-            className="release__image"
-            alt="Featured Book"
-          />
-          <h4 className="release__title">{book.title}</h4>
-          <div className="release__author">{book.author}</div>
-          <div className="release__price">{book.price}</div>
-          <a href="/" className="release__button">
-            Add to cart
-          </a>
-        </div>
-      ))}
-    </div>
+    <>
+      <h5 className="new-realise-subtitle">Some quality items</h5>
+      <h2 className="new-realise-title">New release books</h2>
+      <div className="release-container">
+        {books.map((book, id) => (
+          <div key={id} className="release">
+            <img
+              src={book.image}
+              className="release__image"
+              alt="Featured Book"
+            />
+            <h4 className="release__title">{book.title}</h4>
+            <div className="release__author">{book.author}</div>
+            <div className="release__price">{book.price}</div>
+            <a href="/" className="release__button">
+              Add to cart
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };

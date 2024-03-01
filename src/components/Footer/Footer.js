0c0944b5c546c2a5e2e8c8a48ae377e267d1f257
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-03-01 20:40:59 +0100

    added fotter menu content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,30 +9,58 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
+  const menuItems = [
+    "Home",
+    "About us",
+    "Books",
+    "New release",
+    "Contact us",
+    "Blog",
+  ];
+
   return (
-    <div className="footer-container">
+    <footer className="footer-container">
       <div className="footer-info">
-      <div className="footer-info__image">
-        <img src={footerLogo} alt="Logo" />
-      </div>
+        <div className="footer-info__image">
+          <img src={footerLogo} alt="Logo" />
+        </div>
         <p className="footer-info__text">
-        Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
         </p>
         <div className="footer-info__social">
-        <a href="https://www.facebook.com" className="footer-info__social-icon">
-          <FontAwesomeIcon icon={faFacebookF} />
-        </a>
-        <a href="https://www.instagram.com" className="footer-info__social-icon">
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        <a href="https://twitter.com/" className="footer-info__social-icon">
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
-        <a href="https://www.youtube.com/" className="footer-info__social-icon">
-          <FontAwesomeIcon icon={faYoutube} />
-        </a>
+          <a
+            href="https://www.facebook.com"
+            className="footer-info__social-icon"
+          >
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+          <a
+            href="https://www.instagram.com"
+            className="footer-info__social-icon"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="https://twitter.com/" className="footer-info__social-icon">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a
+            href="https://www.youtube.com/"
+            className="footer-info__social-icon"
+          >
+            <FontAwesomeIcon icon={faYoutube} />
+          </a>
+        </div>
       </div>
+
+      <div className="footer-menu">
+        <h5 className="footer-menu__title">Company</h5>
+        <ul className="footer-menu__list">
+          {menuItems.map((menuItem) => (
+            <li key={menuItem}>{menuItem}</li>
+          ))}
+        </ul>
       </div>
-    </div>
+    </footer>
   );
 };

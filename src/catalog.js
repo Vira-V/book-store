fe9d2463abd7a6338b6e20cbde3b4ec928c17840
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-03-12 18:04:56 +0100

    created data for Book catalog

---
+++ src/catalog.js
@@ -0,0 +1,118 @@
+import image1 from "../src/images/book1.png";
+import image2 from "../src/images/catalog2.png";
+import image3 from "../src/images/catalog3.png";
+import image4 from "../src/images/book2.png";
+import image5 from "../src/images/catalog5.png";
+import image6 from "../src/images/book3.png";
+import image7 from "../src/images/book4.png";
+import image8 from "../src/images/catalog8.png";
+import image9 from "../src/images/catalog9.png";
+import image10 from "../src/images/catalog10.png";
+
+export const books = [
+  {
+    id: 1,
+    image: image1,
+    title: "Simple Way Of Piece Life",
+    author: "Armor Ramsey",
+    price: "$ 40.00",
+  },
+  {
+    id: 2,
+    image: image2,
+    title: "Great Travel At Desert",
+    author: "Sanhii Howly",
+    price: "$ 38.00",
+  },
+  {
+    id: 3,
+    image: image3,
+    title: "The Lade beauty Scarlett",
+    author: "Arthur Doyle",
+    price: "$ 45.00",
+  },
+  {
+    id: 4,
+    image: image4,
+    title: "Great travel at desert",
+    author: "Sanchit Howdy",
+    price: "$ 38.00",
+  },
+  {
+    id: 5,
+    image: image5,
+    title: "The hypocrite world",
+    author: "Sophia Hill",
+    price: "$ 45.00",
+  },
+  {
+    id: 6,
+    image: image6,
+    title: "The lady beauty scarlett",
+    author: "Arthur Doyle",
+    price: "$ 45.00",
+  },
+  {
+    id: 7,
+    image: image7,
+    title: "Once upon a time",
+    author: "Killen Marry",
+    price: "$ 35.00",
+  },
+  {
+    id: 8,
+    image: image8,
+    title: "The road to recognition",
+    author: "Barry Feldman",
+    price: "$ 49.00",
+  },
+  {
+    id: 9,
+    image: image9,
+    title: "The mind connection",
+    author: "Joyce Meyer",
+    price: "$ 41.00",
+  },
+  {
+    id: 10,
+    image: image10,
+    title: "Battlefield of the mind",
+    author: "Joyce Meyer",
+    price: "$ 41.50",
+  },
+  {
+    id: 11,
+    image: image4,
+    title: "Great travel at desert",
+    author: "Sanchit Howdy",
+    price: "$ 38.00",
+  },
+  {
+    id: 12,
+    image: image5,
+    title: "The hypocrite world",
+    author: "Sophia Hill",
+    price: "$ 45.00",
+  },
+  {
+    id: 13,
+    image: image7,
+    title: "Once upon a time",
+    author: "Killen Marry",
+    price: "$ 35.00",
+  },
+  {
+    id: 14,
+    image: image8,
+    title: "The road to recognition",
+    author: "Barry Feldman",
+    price: "$ 49.00",
+  },
+  {
+    id: 15,
+    image: image3,
+    title: "The Lade beauty Scarlett",
+    author: "Arthur Doyle",
+    price: "$ 45.00",
+  },
+];

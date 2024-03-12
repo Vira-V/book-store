7e2fccc6929bc3ef123590f72c4bedd993e25459
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-03-12 18:28:15 +0100

    added submenu for catalog

--- src/components/BookCatalog/BookCatalog.js
+++ src/components/BookCatalog/BookCatalog.js
@@ -1,25 +1,37 @@
 import "./BookCatalog.scss";
+import { NavLink } from "react-router-dom";
 
 export const BookCatalog = ({ catalog }) => {
-  return (
-    <>
-      <div className="release-container">
-        {catalog.map((catalog, id) => (
-          <div key={id} className="release">
-            <img
-              src={catalog.image}
-              className="release__image"
-              alt="Featured Book"
-            />
-            <h4 className="release__title">{catalog.title}</h4>
-            <div className="release__author">{catalog.author}</div>
-            <div className="release__price">{catalog.price}</div>
-            <a href="/" className="release__button">
-              Add to cart
-            </a>
-          </div>
-        ))}
-      </div>
-    </>
-  );
-};
+    return (
+      <>
+        <div className="catalog-navigation">
+          <NavLink to="/">
+            <p>Home</p>
+          </NavLink>
+          <span>/</span>
+          <NavLink to="/books" className="navigation__category-link">
+            <p>Books</p>
+          </NavLink>
+        </div>
+  
+        <div className="release-container">
+          {catalog.map((book, id) => (
+            <div key={id} className="release">
+              <img
+                src={book.image}
+                className="release__image"
+                alt="Featured Book"
+              />
+              <h4 className="release__title">{book.title}</h4>
+              <div className="release__author">{book.author}</div>
+              <div className="release__price">{book.price}</div>
+              <a href="/" className="release__button">
+                Add to cart
+              </a>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  };
+  

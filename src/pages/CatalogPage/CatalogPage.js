794820f85e4c1180d60af29f05e5f36a99ca539c
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-03-15 16:10:01 +0100

    added Pagination component to Catalog Page

--- src/pages/CatalogPage/CatalogPage.js
+++ src/pages/CatalogPage/CatalogPage.js
@@ -1,6 +1,30 @@
+import React, { useState } from 'react';
 import { catalog } from "../../catalog";
 import { BookCatalog } from "../../components/BookCatalog/BookCatalog";
+import Pagination from "../../components/Pagination/Pagination";
 
-export const CatalogPage = () => (
-    <BookCatalog catalog={catalog}/>
-  )
+export const CatalogPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(6);
+  const total = catalog.length;
+
+  const startIndex = (currentPage - 1) * booksPerPage;
+  const endIndex = startIndex + booksPerPage;
+  const displayedBooks = catalog.slice(startIndex, endIndex);
+
+  const handlePageSwitch = (page) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <>
+      <BookCatalog catalog={displayedBooks} />
+      <Pagination
+        onPageChange={handlePageSwitch}
+        totalCount={total}
+        pageSize={booksPerPage}
+        currentPage={currentPage}
+      />
+    </>
+  );
+};

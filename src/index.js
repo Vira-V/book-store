4ee97ae9ad4ec8ab7bfe85b93bc26f5b0cf84a0e
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-03-25 22:13:40 +0100

    created Redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import booksReducer from "./features/booksSlice";
+
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+  },
+});
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </HashRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );

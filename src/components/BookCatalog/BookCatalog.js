75e17c0e797e0762687005022cf4970159db09cf
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-03-27 20:05:46 +0100

    rewrite BookCatalog component after adding RTK Query

--- src/components/BookCatalog/BookCatalog.js
+++ src/components/BookCatalog/BookCatalog.js
@@ -1,7 +1,9 @@
 import "./BookCatalog.scss";
 import { NavLink } from "react-router-dom";
+import { useGetAllBooksQuery } from "../../features/booksApi";
 
-export const BookCatalog = ({ catalog }) => {
+export const BookCatalog = () => {
+  const { data, error, isLoading } = useGetAllBooksQuery();
   return (
     <>
       <div className="catalog-navigation">
@@ -15,21 +17,27 @@ export const BookCatalog = ({ catalog }) => {
       </div>
 
       <div className="catalog-container">
-        {catalog.map((book, id) => (
-          <div key={id} className="catalog">
-            <img
-              src={book.image}
-              className="catalog__image"
-              alt="Featured Book"
-            />
-            <h4 className="catalog__title">{book.title}</h4>
-            <div className="catalog__author">{book.author}</div>
-            <div className="catalog__price">{book.price}</div>
-            <a href="/" className="catalog__button">
-              Add to cart
-            </a>
-          </div>
-        ))}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>An error ...</p>
+        ) : (
+          data.map((book, id) => (
+            <div key={id} className="catalog">
+              <img
+                src={book.image}
+                className="catalog__image"
+                alt="Featured Book"
+              />
+              <h4 className="catalog__title">{book.title}</h4>
+              <div className="catalog__author">{book.author}</div>
+              <div className="catalog__price">{book.price}</div>
+              <a href="/" className="catalog__button">
+                Add to cart
+              </a>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
